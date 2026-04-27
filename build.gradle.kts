538d8f5a96a6a43fd0b68e9a3f11b5c95196d4ac
ghastlith <72772611+ghastlith@users.noreply.github.com>
2026-04-27 19:33:27 -0300

    init: add barebones project code

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+  java
+  id("org.springframework.boot") version "4.0.6"
+  id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "ghastlith"
+version = "1.0.0"
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  // spring
+  implementation("org.springframework.boot:spring-boot-starter")
+
+  // tests
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+  // lombok
+  compileOnly("org.projectlombok:lombok:1.18.38")
+  annotationProcessor("org.projectlombok:lombok:1.18.38")
+  testCompileOnly("org.projectlombok:lombok:1.18.38")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
+}
+
+tasks.withType<Test>().configureEach {
+  useJUnitPlatform()
+
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
+}
