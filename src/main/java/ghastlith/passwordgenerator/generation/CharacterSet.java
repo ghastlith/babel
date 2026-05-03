f79191ef2eb2d31464648b523d4ec1fd944664fd
ghastlith <72772611+ghastlith@users.noreply.github.com>
2026-05-02 22:48:44 -0300

    feat: add character set for password generation

---
+++ src/main/java/ghastlith/passwordgenerator/generation/CharacterSet.java
@@ -0,0 +1,9 @@
+package ghastlith.passwordgenerator.generation;
+
+public class CharacterSet {
+
+  public static final String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  public static final String NUMBERS = "0123456789";
+  public static final String SPECIAL = "!#$%&()*+,-./:;<=>?@[]^_{}~";
+
+}
